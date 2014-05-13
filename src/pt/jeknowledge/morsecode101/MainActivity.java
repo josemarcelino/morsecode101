33eb4a0ad35d4644b4eb59158a74b62ee48f0aff
josemarcelino <jose.m.s.marcelino@gmail.com>
2014-05-13 05:38:51 +0100

    basic listenner text

--- src/pt/jeknowledge/morsecode101/MainActivity.java
+++ src/pt/jeknowledge/morsecode101/MainActivity.java
@@ -2,7 +2,10 @@ package pt.jeknowledge.morsecode101;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +13,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		final Button button= (Button) findViewById(R.id.refreshButton);
+		final TextView settext = (TextView) findViewById(R.id.textView);
+		final EditText rectext = (EditText) findViewById(R.id.editText);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        
+		    	settext.setText((CharSequence) rectext.getText());
+		    	
+		    	
+		    }
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	
+	
 }
