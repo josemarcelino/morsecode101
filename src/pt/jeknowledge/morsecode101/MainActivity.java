68309473abb1e542c5b64a19dba77261175e0a1a
josemarcelino <jose.m.s.marcelino@gmail.com>
2014-05-14 23:50:25 +0100

    adding Alphabet and Letter Classes

--- src/pt/jeknowledge/morsecode101/MainActivity.java
+++ src/pt/jeknowledge/morsecode101/MainActivity.java
@@ -2,6 +2,7 @@ package pt.jeknowledge.morsecode101;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,17 +20,38 @@ public class MainActivity extends Activity {
 		final TextView settext = (TextView) findViewById(R.id.textView);
 		final EditText rectext = (EditText) findViewById(R.id.editText);
 		
+		
+		
 		button.setOnClickListener(new View.OnClickListener() {
 		    @Override
 		    public void onClick(View v) {
-		        
-		    	settext.setText((CharSequence) rectext.getText());
 		    	
+		    	String before = (String) settext.getText();
+		    	String after = convertToMorse(before);
+		    	settext.setText(after);
 		    	
 		    }
 		});
 	}
 
+	private String convertToMorse(String before) {
 	
+		
+		
+		String [] after  = new String [before.length()];
+		
+		for(int i = 0; i <before.length(); i++){
+			
+			
+			after[i] = Alphabet.getEquiv(before.charAt(i));
+			
+			
+		}
+			
+		
+		
+		String afterConcat = null;
+		return afterConcat;
+	}
 	
 }
